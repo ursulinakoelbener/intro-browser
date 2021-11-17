b6107ca13dea7eb4977ba9b2b7991dc40b2092df
Stefano Balietti <futur.dorko@gmail.com>
2021-11-17 15:48:31 +0100

    refactor and attributes

---
+++ 3_Browser/js/external_scripts.js
@@ -0,0 +1,11 @@
+// Uncomment for step-by-step execution.
+// debugger;
+
+// Return the element with ID "above".   
+let above = document.querySelector('#above');
+
+above.innerHTML += ' is above.';
+
+let below = document.querySelector('#below');
+
+below.innerHTML += ' is below.';
